32b3480799994fc90615a698bf503a3a450fb6b0
ljhxksy <anh.hoang649053@tufts.edu>
2023-08-14 15:40:40 +0700

    initialize admin landing page

--- app/helpers/admin/session_helper.rb
+++ app/helpers/admin/session_helper.rb
@@ -2,7 +2,7 @@ module Admin::SessionHelper
     def current_admin
         if session[:user_id]
             @current_user ||= User.find_by(id: session[:user_id])
-            return nil if @current_user.user?
+            @current_user.admin? ? @current_user : nil 
         end
     end
 
